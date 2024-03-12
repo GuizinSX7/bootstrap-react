4ef1c4673fb53b99d3574424b6f7eff50a30cfc6
GuizinSX7 <guilhermeferraresidallacqua405@gmail.com>
2024-03-12 17:03:54 -0300

    fdsfdsfds

--- react-bootstrap/src/pages/Menu.jsx
+++ react-bootstrap/src/pages/Menu.jsx
@@ -9,7 +9,7 @@ function Menu() {
     <div className="navbar">
       <Navbar expand="lg">
         <Container>
-          <Navbar.Brand href="#home">Climeos</Navbar.Brand>
+          <Navbar.Brand href="/">Climeos</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
